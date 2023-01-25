95bece095f6f6b551866c6dc200272a27f797001
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2023-01-25 21:20:00 +0300

    Operators ++ done

--- IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/Source.cpp
@@ -61,12 +61,27 @@ public:
 	}
 
     //                   Operators:
-	Point operator=(const Point& other)
+	Point& operator=(const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
+	}
+
+	Point& operator++()
+	{
+		x++;
+		y++;
+		return *this;
+	}
+
+	Point operator++(int)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
 	}
 
 	//                    Methods:
@@ -89,6 +104,14 @@ double distance(const Point& A, Point& B)
 	double y_distance = A.get_y() - B.get_y();
 	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 	return distance;
+}
+
+Point operator+(const Point& left, const Point& right)
+{
+	Point res;
+	res.set_x(left.get_x() + right.get_x());
+	res.set_y(left.get_y() + right.get_y());
+	return res;
 }
 
 //#define STRUCT_POINT
@@ -164,9 +187,29 @@ void main()
 	C.print();
 #endif // ASSIGNMENT_CHECK_1
 
+#ifdef ASSIGNMENT_CHECK_2
 	int a, b, c;
 	a = b = c = 0;
 
 	Point A, B, C;
 	A = B = C = Point(2, 3);
+	//Point(2, 3) - явный вызов конструктора, и этот конструктор создает временный безымянный объект
+	//Временные безымянные объекты существуют в пределах одного выражения  
+#endif // ASSIGNMENT_CHECK_2
+
+
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3);
+	Point B(4, 5);
+	Point C = A + B;
+	C.print();
+	cout << delimeter << endl;
+	++C;
+	C.print();
+	cout << delimeter << endl;
+	C++;
+	C.print();
 }
