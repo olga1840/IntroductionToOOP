4ebe6324fc95131f52f345a8f74b2778aaff909f
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2023-02-08 16:09:13 +0300

    String 1

---
+++ String/Source.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class String
+{
+	int size;		//размер строки
+	char* str;		//адрес строки в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	//				Constructors:
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstructor:\t" << this << endl;
+	}
+
+	~String()
+	{
+		delete this->str;
+		cout << "Destructor:\t" << this << endl;
+	}
+	std::ostream& operator<<(std::ostream& os, const String& obj)
+	{
+		return obj.print(os);
+	}
+
+	//				Methods:
+	std::ostream& print(std::ostream& os)const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*String str1(5);
+	str1.print();*/
+
+	String str1 = "Hello";
+	cout << str1 << endl;
+}
