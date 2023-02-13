bec1d7de831d04c3137ac1720a46a89e4216690b
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2023-02-13 22:16:20 +0300

    Fraction separation done

--- Fraction/Fraction.h
+++ Fraction/Fraction.h
@@ -12,7 +12,8 @@ Fraction operator*(Fraction left, Fraction right);
 Fraction operator/(const Fraction& left, const Fraction& right);
 Fraction operator+(Fraction left, Fraction right);
 Fraction operator-(Fraction left, Fraction right);
-
+std::ostream& operator<<(std::ostream& os, const Fraction& fraction);
+std::istream& operator>>(std::istream& in, Fraction& fraction);
 
 class Fraction
 {
